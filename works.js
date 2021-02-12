551b7cc8fe5aa40da1681ff581c8ac28f6766321
khavan <sam.khavan.tran@gmail.com>
2021-02-12 00:43:29 -0500

    Update works.js

--- works.js
+++ works.js
@@ -22,41 +22,43 @@ function winScroll() {
         let c2Start = secC2.offset().top;
         let c3Start = secC3.offset().top;
         let footStart = secFoot.offset().top;
-if (screenLandscapeTop()) {
-        if (wScroll >= -20 && wScroll < headStart + headLength - windowH*titleChange) {
-            block.css("background-color", "#D4DCDE");
-            mainP.css({"color":"#000000"});
+
+        if (screenLandscapeTop()) {
+            if (wScroll >= -20 && wScroll < headStart + headLength - windowH*titleChange) {
+                block.css("background-color", "#D4DCDE");
+                mainP.css({"color":"#000000"});
+                typeWriter();
+            }
+            else if (wScroll >= headStart + headLength - windowH*titleChange && wScroll < c1Start + c1Length - windowH*titleChange) {
+                // case1
+                block.css("background-color", "#000");
+                mainP.css({"color":"#F6F6F6"});
+                
+            }
+            else if (wScroll >= c1Start + c1Length - windowH*titleChange && wScroll < c2Start + c2Length - windowH*titleChange) {
+                // case2
+                block.css("background-color", "#FFE66A");
+                mainP.css({"color":"#4303A7"});
+                
+            }
+            else if (wScroll >= c2Start + c2Length - windowH*titleChange && wScroll < c3Start + c3Length - windowH*titleChange) {
+                // case3
+                block.css("background-color", "#FCFAF7");
+                mainP.css({"color":"#295849"});
+                $('works_title').removeClass('word_enter');
+            }
+            else if (wScroll >= c3Start + c3Length - windowH*titleChange && wScroll < footStart + footLength - windowH*titleChange) {
+                // footer
+                block.css("background-color", "#f6f6f6");
+                mainP.css({"color":"#000000"});
+            }
+            else {
+                block.css("background-color", "#f6f6f6");
+            }
+        } else {
             typeWriter();
         }
-        else if (wScroll >= headStart + headLength - windowH*titleChange && wScroll < c1Start + c1Length - windowH*titleChange) {
-            // case1
-            block.css("background-color", "#000");
-            mainP.css({"color":"#F6F6F6"});
-            
-        }
-        else if (wScroll >= c1Start + c1Length - windowH*titleChange && wScroll < c2Start + c2Length - windowH*titleChange) {
-            // case2
-            block.css("background-color", "#FFE66A");
-            mainP.css({"color":"#4303A7"});
-            
-        }
-        else if (wScroll >= c2Start + c2Length - windowH*titleChange && wScroll < c3Start + c3Length - windowH*titleChange) {
-            // case3
-            block.css("background-color", "#FCFAF7");
-            mainP.css({"color":"#295849"});
-            $('works_title').removeClass('word_enter');
-        }
-        else if (wScroll >= c3Start + c3Length - windowH*titleChange && wScroll < footStart + footLength - windowH*titleChange) {
-            // footer
-            block.css("background-color", "#f6f6f6");
-            mainP.css({"color":"#000000"});
-        }
-        else {
-            block.css("background-color", "#f6f6f6");
-        }
-    }
     }));
-    }
 }
 (function($) {
     typeWriter();
