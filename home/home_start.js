17bc50e22fb188ca912111b8e2f7466016893a1e
khavan <sam.khavan.tran@gmail.com>
2021-02-12 14:20:30 -0500

    Update home_start.js

--- home/home_start.js
+++ home/home_start.js
@@ -9,13 +9,12 @@ $(window).on('load', function(){
     const navPseudoSVG = $(".nav_pseudo_container svg path");
 
     navLogo_cont.addClass('nav_start');
-    navHead.addClass('nav_start').css({'opacity':1});
     navLogo_icon.addClass('nav_start');
     navSelect.addClass('nav_start');
     nav.addClass('nav_start');
     navPseudo.addClass("nav_start");
     navPseudoSVG.css({"fill":"#000"});
-    
+    navHead.addClass('nav_start').css({"font-size": (startSizing()-logoFinalSize) + logoFinalSize + "px", "opacity":1});
     navLogo_box.addClass('nav_start').css({"grid-template-columns": "1fr " + 0 + "px","-ms-grid-columns": "1fr " + 0 + "px"});
     navPseudo.addClass("nav_start").css({"margin-left": 0 + "px", "opacity":1});
 
