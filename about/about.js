4be4d0c9d7648b41589e6df2495aa6fa8932bd58
khavan <sam.khavan.tran@gmail.com>
2021-02-23 12:49:43 -0500

    Update about.js

--- about/about.js
+++ about/about.js
@@ -1,28 +1,65 @@
 const aboutLogo = $(".image_carousel_container");
 //sections
 const aboutHead = $('#about_header');
-const aboutDes = $('#about_description');
 const aboutIll = $('#about_illustration');
+const aboutTag = $('#about_tagline');
+const aboutDes = $('#about_description');
 const aboutSkill = $('#about_skills');
 const aboutWork = $('#about_work');
+const aboutFoot = $('#sec_footer');
 //intro text
 const introText = $('.variable_word span');
-
+const mainP = $('.about_colour');
 function winScroll() {
+
+    // Heights
+    let headLength = aboutHead.innerHeight();
+    let illLength = aboutIll.innerHeight();
+    let tagLength = aboutTag.innerHeight();
+    let desLength = aboutDes.innerHeight();
+    let skillLength = aboutSkill.innerHeight();
+    let workLength = aboutWork.innerHeight();
+    let footLength = aboutFoot.innerHeight();
 
     // Heights
     let abHeadLength = aboutHead.innerHeight();
     $(window).scroll($.throttle( 10,function() {
         let wScroll = $(window).scrollTop();
 
-        //position information
+        //position information || - windowH*titleChange
         let abHeadStart = aboutHead.offset().top;
 
-        if (wScroll >= -20 && wScroll < abHeadStart + abHeadLength - windowH*titleChange) {
+        if (wScroll >= -20 && wScroll < abHeadStart + abHeadLength ) {
             typeWriter();
-        } else {
+            mainP.css('color','#fff');
+            block.css("background-color", "#000"); 
+        } 
+        else if (wScroll >= headStart + headLength && wScroll < illStart + illLength - windowH*titleChange  ) {
             introText.removeClass("word_enter");
+            mainP.css('color','#fff');
+            block.css("background-color", "#000"); 
         }
+        else if (wScroll >= illStart + illLength - windowH*titleChange  && wScroll < tagStart + tagLength - windowH*titleChange  ) {
+            mainP.css('color','#fff');
+            block.css("background-color", "#000"); 
+        }
+        else if (wScroll >= tagStart + tagLength - windowH*titleChange  && wScroll < desStart + desLength - windowH*titleChange  ) {
+            mainP.css('color','#000');
+            block.css("background-color", "#FFFFFF"); 
+        }
+        else if (wScroll >= desStart + desLength - windowH*titleChange  && wScroll < skillStart + skillLength  - windowH*titleChange ) {
+            mainP.css('color','#000');
+            block.css("background-color", "#FFFFFF"); 
+        }
+        else if (wScroll >= skillStart + skillLength - windowH*titleChange  && wScroll < workStart + workLength  - windowH*titleChange ) {
+            mainP.css('color','#000');
+            block.css("background-color", "#FFFFFF"); 
+        }
+        else if (wScroll >= workStart + workLength - windowH*titleChange  && wScroll < footStart + footLength  - windowH*titleChange ) {
+            mainP.css('color','#fff');
+            block.css("background-color", "#000"); 
+        }
+
     }));
 }
 (function($) {
