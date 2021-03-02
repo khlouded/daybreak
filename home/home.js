816cf7398b28524d49a3d14864764564cc7db19b
khavan <sam.khavan.tran@gmail.com>
2021-03-01 21:22:14 -0500

    Update home.js

--- home/home.js
+++ home/home.js
@@ -132,6 +132,11 @@ function worksTransition() {
         worksTransition();
         $(window).scroll($.throttle( 10,function() {
             winScroll();
+            setTimeout(function () {
+                $('.home_h1_title').css({"z-index":"9"});
+                $('.studio_reel').css({"z-index":"9"});
+                $('.heading_item').css({"z-index":"9"});
+            }, 400);
         }));
         footerReveal();
         $(window).resize(function() {
