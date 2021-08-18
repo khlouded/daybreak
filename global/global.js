1087b8eb753653e5068bfa1404cc788d980291f1
khavan <sam.khavan.tran@gmail.com>
2021-08-17 21:05:31 -0400

    trying to fix smoothscroll function

--- global/global.js
+++ global/global.js
@@ -274,6 +274,7 @@ function smoothScroll() {
     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         anchor.addEventListener('click', function (e) {
             e.preventDefault();
+    
             document.querySelector(this.getAttribute('href')).scrollIntoView({
                 behavior: 'smooth'
             });
